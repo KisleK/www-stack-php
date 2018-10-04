ff3b54225b1c093b606fc5bf971b11007bfc4d5f
KisleK <kisle.kuhn1@gmail.com>
2018-10-04 15:41:36 -0500

    loops.php

---
+++ Chapter-5/loops.php
@@ -0,0 +1,58 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: kkuhn
+ * Date: 10/4/2018
+ * Time: 3:28 PM
+ */
+?>
+
+
+<html>
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Loops</title>
+</head>
+<body>
+<?php
+//    $i= 0;
+//
+//    while($i < 101)
+//    {
+//        echo($i);
+//        $i+= 10;
+//        echo("</br>");
+//    }
+//
+//    $airlines = array (
+//            "American",
+//            "Southwest",
+//            "Delta",
+//            "US Airlines",
+//            "United",
+//            "JetBlue",
+//            "Frontier"
+//    );
+//        $x = 0;
+//
+//        while($x < count($airlines))
+//        {
+//            echo ($airlines[$x]);
+//            echo("</br>");
+//            $x++;
+//        }
+
+        $y = 100;
+
+        do
+        {
+            echo($y);
+            echo("</br>");
+            $y++;
+        } while ($y <= 35)
+?>
+</body>
+</html>
