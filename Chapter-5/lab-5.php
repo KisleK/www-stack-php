a9b88260e9a08a057ed83ee6bb6446801b79847e
KisleK <kisle.kuhn1@gmail.com>
2018-10-05 17:22:47 -0500

    start of lab-5

---
+++ Chapter-5/lab-5.php
@@ -0,0 +1,34 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: kkuhn
+ * Date: 10/5/2018
+ * Time: 4:30 PM
+ */
+?>
+
+<html>
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Lab 5</title>
+</head>
+<body>
+    <?php
+            for($x=0; $x < 13; $x++){
+                for($i=0; $i<$x; $i++)
+                {
+                    echo("*");
+                }
+                echo("<br/>");
+
+            }
+
+    ?>
+
+
+
+</body>
+</html>
